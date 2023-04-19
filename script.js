d5197f7b94ce2f9c0e8c34b97c4616a9016af174
nikolacox <130216560+nikolacox@users.noreply.github.com>
2023-04-19 22:41:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "300px";
+  document.getElementById("mySidepanel").style.display = "block";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
-  document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.display = "none";
 }
